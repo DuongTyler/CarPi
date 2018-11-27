143231f76dc79edb73eaaf05834ab8fa5520e63b
Tyler <duongt.sbc@gmail.com>
2018-11-27 02:56:52 +0000

    added initial files

---
+++ scripts/loopps-ef.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+while (true)
+do
+	ps -ef |grep a.out
+	sleep 1
+done
+
---
+++ scripts/playingInfo.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#ncmpcpp -s visualizer
+
+while (true)
+do
+	ncmpcpp --current-song='%t - %l					'
+done
